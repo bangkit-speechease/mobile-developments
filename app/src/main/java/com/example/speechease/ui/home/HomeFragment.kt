3760e2af1d08f904887bd829f1c08a8a770d96ed
OreNanda <dhitananda0219@gmail.com>
2024-11-30 11:08:54 +0700

    New UI

--- app/src/main/java/com/example/speechease/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/speechease/ui/home/HomeFragment.kt
@@ -1,34 +1,55 @@
 package com.example.speechease.ui.home
 
+import android.content.Intent
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import com.example.speechease.R
 import com.example.speechease.databinding.FragmentHomeBinding
+import com.example.speechease.ui.interactive.InteractiveActivity
+import com.example.speechease.ui.practice.PracticeActivity
+import com.example.speechease.ui.progress.ProgressFragment
 
 class HomeFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = HomeFragment()
-    }
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     private val viewModel: HomeViewModel by viewModels()
-    private lateinit var binding: FragmentHomeBinding
-    private lateinit var homeViewModel: HomeViewModel
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.progress.setOnClickListener {
+            val intent = Intent(requireContext(), ProgressFragment::class.java)
+            startActivity(intent)
+        }
+
+        binding.cardLatihan.setOnClickListener {
+            val intent = Intent(requireContext(), PracticeActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.cardInteraktif.setOnClickListener {
+            val intent = Intent(requireContext(), InteractiveActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/example/speechease/ui/interactive/InteractiveViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.speechease.ui.interactive
+
+class InteractiveViewModel {
+}
